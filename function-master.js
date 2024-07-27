99c056cdea4f8ff1c21387add13601ca6e5346d6
Wlegard <whitleylegard@yahoo.com>
2024-07-27 17:38:50 -0500

    function master

--- function-master.js
+++ function-master.js
@@ -35,6 +35,7 @@ function valuesToString(object) {
 //////////////////////////////////////////////////////////////////////
 
 function arrayOrObject(collection) {
+    // returns if an array or an object
     return Array.isArray(collection) ? 'array': 'object';
       
     }
@@ -45,17 +46,21 @@ function arrayOrObject(collection) {
 //////////////////////////////////////////////////////////////////////
 
 function capitalizeWord(string) {
-    return string[0].toUpperCase + string.slice(1);
-    
-}
+    //return the first letter and concatenates it with the rest of the string.
+    return string[0].toUpperCase() + string.slice(1);
+
+}   
 
 //////////////////////////////////////////////////////////////////////
 // Function 6 - Capitalize All Words /////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 function capitalizeAllWords(string) {
+    // returns split string with space
     return string.split(' ')
+    // use capitalizeWord function to each word in the array.
     .map(word => capitalizeWord(word))
+    //join with space
     .join(' ');
 }
 
@@ -64,7 +69,8 @@ function capitalizeAllWords(string) {
 //////////////////////////////////////////////////////////////////////
 
 function welcomeMessage(object) {
-
+    // Capitalizes the first letter of a string.
+    return `Welcome ${capitalizeWord(object.name)}!`;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -72,7 +78,16 @@ function welcomeMessage(object) {
 //////////////////////////////////////////////////////////////////////
 
 function profileInfo(object) {
-
+    //gets the first character of the name property and change it to uppercase.
+    var name = object.name.charAt(0).toUpperCase()
+    //gets the rest of the name string after the first character.
+     + object.name.slice(1);
+         //gets the first character of the species property and change it to uppercase.
+    var species = object.species.charAt(0).toUpperCase() 
+     //gets the rest of the species string after the first character.
+    + object.species.slice(1);
+    // returns the capitalized name, the string " is a ", and the capitalized species.
+    return name + " is a " + species;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -80,14 +95,26 @@ function profileInfo(object) {
 //////////////////////////////////////////////////////////////////////
 
 function maybeNoises(object) {
-
-}
+    //if object and noises key have a noises array
+    if (object.noises && object.noises.length > 0) {
+        // reut
+        // return arrray as a string
+        return object.noises.join(' ');
+        // if no noises, 
+      } else {
+        //return "there are no noises"
+        return "there are no noises";
+      }
+    }
+        
 
 //////////////////////////////////////////////////////////////////////
 // Function 10 - Has Words ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 function hasWord(string, word) {
+    // split string , return true if include word is includes in string of words
+    return string.split(' ').includes(word);
 
 }
 
@@ -96,7 +123,10 @@ function hasWord(string, word) {
 //////////////////////////////////////////////////////////////////////
 
 function addFriend (name, object) {
-
+    //access the object and the friends array and add name
+object.friends.push(name);
+//return the object
+return object;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -104,6 +134,8 @@ function addFriend (name, object) {
 //////////////////////////////////////////////////////////////////////
 
 function isFriend(name, object) {
+    // return the friends oject the name is included in friends, false if not
+    return object.friends?.includes(name) ?? false;
 
 }
 
@@ -120,14 +152,23 @@ function nonFriends(name, array) {
 //////////////////////////////////////////////////////////////////////
 
 function updateObject(object, key, value) {
+    // accessing the key through the object to give it new value
+    object[key] = value;
+    //return object
+    return object;
+  
 
 }
-
 //////////////////////////////////////////////////////////////////////
 // Function 15 - Remove Properties ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 function removeProperties(object, array) {
+    //This method iterates over each element in the array.
+    array.forEach(function(prop) {
+        // removes the property specified by prop from the object
+        delete object[prop];
+      });
 
 }
 
@@ -136,7 +177,9 @@ function removeProperties(object, array) {
 //////////////////////////////////////////////////////////////////////
 
 function dedup(array) {
-
+    //return using the filter method to remove duplicates
+    return array.filter((item,
+        index) => array.indexOf(item) === index);
 }
 
 //////////////////////////////////////////////////////////////////////
